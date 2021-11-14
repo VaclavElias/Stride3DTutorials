00803b991888b125abf6efea451d8a4fe5046b6d
Vaclav Elias <vaclavelias@gmail.com>
2021-11-14 18:34:02 +0000

    Code cleanup

--- UI/DragAndDrop/DragAndDrop/DragAndDropContainer.cs
+++ UI/DragAndDrop/DragAndDrop/DragAndDropContainer.cs
@@ -18,7 +18,6 @@ namespace DragAndDrop
             PreviewTouchUp += MainCanvas_PreviewTouchUp;
         }
 
-        // we need to increase ZIndex so the active or a new window is on the top, let's hope you will close the game by the time this hits the max :)
         public int GetNewZIndex() => _lastZIndex++;
 
         public void SetOffset(Vector2 vector) => _offset = vector;
@@ -27,9 +26,9 @@ namespace DragAndDrop
         
         private void MainCanvas_PreviewTouchMove(object? sender, TouchEventArgs e)
         {
-            if (_dragElement == null) return;
+            //if (_dragElement is null) return;
 
-            _dragElement.SetCanvasRelativePosition((Vector3)(e.ScreenPosition - _offset ?? Vector2.Zero));
+            _dragElement?.SetCanvasRelativePosition((Vector3)(e.ScreenPosition - _offset ?? Vector2.Zero));
         }
 
         private void MainCanvas_PreviewTouchUp(object? sender, TouchEventArgs e)
