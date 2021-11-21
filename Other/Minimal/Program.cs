e5a5375eb184516bcde26e86c005f332d8b92d3f
Vaclav Elias <vaclavelias@gmail.com>
2021-11-21 17:19:51 +0000

    Camera and light added

--- Other/Minimal/Program.cs
+++ Other/Minimal/Program.cs
@@ -7,29 +7,6 @@ namespace Minimal
         {
             using var game = new MyGame2();
 
-            // How do we create programatically a new scene and attatch to Game?
-            //Scene scene = new Scene();
-
-            //var model = new Model();
-            //var cube = new CubeProceduralModel();
-
-            //cube.Generate(game.Services, model);
-
-            //var cubeEntity = new Entity();
-            //cubeEntity.Transform.Scale = new Vector3(1);
-            //cubeEntity.Transform.Position = new Vector3(1);
-
-            //cubeEntity.GetOrCreate<ModelComponent>().Model = model;
-
-            // How do we attach a Sync script? 
-            // This is causing an error
-            // Error: Service [IContentManager] not found'
-            //cubeEntity.Add(new TestComponent());
-
-            //scene.Entities.Add(cubeEntity);
-
-            //game.SceneSystem.SceneInstance = new SceneInstance(game.Services, scene);
-
             game.Run();
         }
     }
