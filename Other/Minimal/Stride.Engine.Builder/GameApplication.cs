30d806a2478432b0fe45dba0615ab2761047f9ff
Vaclav Elias <vaclavelias@gmail.com>
2021-12-05 14:38:37 +0000

    Stride.Engine.Builder added

---
+++ Other/Minimal/Stride.Engine.Builder/GameApplication.cs
@@ -0,0 +1,12 @@
+namespace Stride.Engine.Builder
+{
+    // Or GameEngine?
+    public class GameApplication
+    {
+        // This will bootstrap all boilerplate 
+        public static void CreateBuilder()
+        {
+
+        }
+    }
+}
