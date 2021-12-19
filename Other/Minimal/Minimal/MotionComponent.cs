440995424ce0d8f1668e1677d42cf216aea5a806
Vaclav Elias <vaclavelias@gmail.com>
2021-12-19 11:28:00 +0000

    MotionComponent added

---
+++ Other/Minimal/Minimal/MotionComponent.cs
@@ -0,0 +1,25 @@
+namespace Minimal;
+
+public class MotionComponent : SyncScript
+{
+    Vector3 _initialPosition = Vector3.Zero;
+
+    public override void Start()
+    {
+        Log.Warning("Start Logging");
+
+        _initialPosition = Entity.Transform.Position;
+    }
+
+    public override void Update()
+    {
+
+        Log.Warning("Update Logging");
+
+        if (Vector3.Distance(_initialPosition, Entity.Transform.Position) <= 1)
+        {
+            Entity.Transform.Position.X += 0.03f;
+        }
+    }
+}
+
