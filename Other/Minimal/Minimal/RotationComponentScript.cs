04bf492ea5390a118aee98eded616a909cfca4e9
Vaclav Elias <vaclavelias@gmail.com>
2022-01-08 23:56:02 +0000

    Rotation scrip added

---
+++ Other/Minimal/Minimal/RotationComponentScript.cs
@@ -0,0 +1,25 @@
+namespace Minimal;
+
+public class RotationComponentScript : SyncScript
+{
+    private Vector3 _initialPosition = Vector3.Zero;
+    private float _rotateSpeed = 5f;
+    private float _radius = 1f;
+    private float _angle = 0f;
+
+    public override void Start()
+    {
+        Log.Warning("Start Logging");
+
+        _initialPosition = Entity.Transform.Position;
+    }
+
+    public override void Update()
+    {
+        _angle += _rotateSpeed * (float)Game.UpdateTime.Elapsed.TotalSeconds;
+
+        var offset = new Vector3((float)Math.Sin(_angle), 0, (float)Math.Cos(_angle)) * _radius;
+
+        Entity.Transform.Position = _initialPosition + offset;
+    }
+}
